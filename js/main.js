897aaf1b7e573dde38b90e816efcb45dd7385b7f
Kondo <kondo@sabira.jp>
2021-07-02 09:06:34 +0900

    add filter

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 (() => {
   "use strict";
 
-  $("#random").on("click", () => {
-    
+  $("#random").on("click", () => {});
+
+  let threads = GmailApp.search("Kindle");
+  threads.filter((mail) => {
+    mail.moveToTrash();
   });
 
   let app = new Vue({
