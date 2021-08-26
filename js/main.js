b7b4f148d23b7279c2d5d581693c858c519434cb
Kondo <kondo@sabira.jp>
2021-08-26 14:58:53 +0900

    add vue

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
-(() => {
-  "use strict";
+// onclick
+$('#random').on('click', () => {});
 
-  $("#random").on("click", () => {});
+// filter
+let threads = GmailApp.search('Kindle');
+threads.filter((mail) => {
+  mail.moveToTrash();
+});
 
-  let threads = GmailApp.search("Kindle");
-  threads.filter((mail) => {
-    mail.moveToTrash();
-  });
-
-  let app = new Vue({
-    el: "#app",
-  });
-})();
+// Vue
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      name: '太郎',
+      age: 1,
+    };
+  },
+  created() {
+    console.log('created');
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  methods: {
+    add() {
+      this.age++;
+      console.log('add');
+    },
+  },
+  computed: {
+    now() {
+      return this.age * 2;
+    },
+  },
+  watch: {
+    firstName(newValue, oldValue) {
+      console.log(newValue, oldValue);
+    },
+  },
+});
